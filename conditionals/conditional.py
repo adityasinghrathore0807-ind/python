df2da2c918141a47899a3b8d68c56d0a9da91634
adityasinghrathore0807-ind <adityasingh.rathore0807@gmail.com>
2026-06-19 11:30:44 +0530

    if elif else ladder

---
+++ conditionals/conditional.py
@@ -0,0 +1,10 @@
+a = int(input("enter your age:"))
+if(a>=18):
+    print("you are above the age of consent")
+    print("good")
+elif(a<0):
+    print("you are enter invalid age")
+else:
+    print("you are below the age of consent")
+
+print("end of program")
