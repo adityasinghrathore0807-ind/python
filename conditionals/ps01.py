6b2aeab4428b9664b9baafc3ece62324c32703e0
adityasinghrathore0807-ind <adityasingh.rathore0807@gmail.com>
2026-06-19 11:50:33 +0530

    check gratest no.

---
+++ conditionals/ps01.py
@@ -0,0 +1,12 @@
+a1 = int(input("enter no.01: "))
+a2 = int(input("enter no.02: "))
+a3 = int(input("enter no.03: "))
+a4 = int(input("enter no.04: "))
+if(a1>a2 and a1>a3 and a1>a4):
+    print("a1 is greatesr no.",a1)
+elif(a2>a1 and a2>a3 and a2>a4):
+    print("a2 is greatesr no.",a2)
+elif(a3>a1 and a3>a2 and a3>a4):
+    print("a3 is greatesr no.",a3)
+elif(a4>a1 and a4>a3 and a4>a2):
+    print("a4 is greatesr no.",a4)
